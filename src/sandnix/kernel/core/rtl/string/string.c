5a2bcecffca1a2efbe47c6f065c5d0801832165d
darknightghost <darknightghost.cn@gmail.com>
2016-07-19 13:47:43 +0800

    UTC:2016-07-19  5:47:43 : string

--- src/sandnix/kernel/core/rtl/string/string.c
+++ src/sandnix/kernel/core/rtl/string/string.c
@@ -20,7 +20,7 @@
 
 #include "string.h"
 #include "../../../hal/rtl/rtl.h"
-typedef unsigned long	longword;
+typedef unsigned long	longword_t;
 
 void* core_rtl_memccpy(void* dest, const void* src, u8 ch, size_t size)
 {
@@ -43,37 +43,40 @@ void* core_rtl_memccpy(void* dest, const void* src, u8 ch, size_t size)
 
 void* core_rtl_memchr(const void* buf, u8 ch, size_t size)
 {
+    //Copied from glibc
     /*
-    	On 32-bit hardware, choosing longword to be a 32-bit unsigned
+    	On 32-bit hardware, choosing longword_t to be a 32-bit unsigned
     	long instead of a 64-bit uintmax_t tends to give better
     	performance.  On 64-bit hardware, unsigned long is generally 64
     	bits already.  Change this typedef to experiment with
     	performance.
     */
     u8* char_ptr;
-    const longword *longword_ptr;
-    longword repeated_one;
-    longword repeated_c;
+    const longword_t *longword_t_ptr;
+    longword_t repeated_one;
+    longword_t repeated_c;
 
     /*
     	Handle the first few bytes by reading one byte at a time.
-    	Do this until CHAR_PTR is aligned on a longword boundary.
+    	Do this until CHAR_PTR is aligned on a longword_t boundary.
     */
     for(char_ptr = (u8*)buf;
-        size > 0 && (size_t) char_ptr % sizeof(longword) != 0;
-        --size, ++char_ptr)
-        if(*char_ptr == ch)
+        size > 0 && (size_t) char_ptr % sizeof(longword_t) != 0;
+        --size, ++char_ptr) {
+        if(*char_ptr == ch) {
             return (void *) char_ptr;
+        }
+    }
 
-    longword_ptr = (const longword *) char_ptr;
+    longword_t_ptr = (const longword_t *) char_ptr;
 
     /*
-    	All these elucidatory comments refer to 4-byte longwords,
-    	but the theory applies equally well to any size longwords.
+    	All these elucidatory comments refer to 4-byte longword_ts,
+    	but the theory applies equally well to any size longword_ts.
     */
 
     /*
-    	Compute auxiliary longword values:
+    	Compute auxiliary longword_t values:
     	repeated_one is a value which has a 1 in every byte.
     	repeated_c has ch in every byte.
     */
@@ -81,14 +84,14 @@ void* core_rtl_memchr(const void* buf, u8 ch, size_t size)
     repeated_c = ch | (ch << 8);
     repeated_c |= repeated_c << 16;
 
-    if(0xffffffffU < (longword) - 1) {
+    if(0xffffffffU < (longword_t) - 1) {
         repeated_one |= repeated_one << 31 << 1;
         repeated_c |= repeated_c << 31 << 1;
 
-        if(8 < sizeof(longword)) {
+        if(8 < sizeof(longword_t)) {
             size_t i;
 
-            for(i = 64; i < sizeof(longword) * 8; i *= 2) {
+            for(i = 64; i < sizeof(longword_t) * 8; i *= 2) {
                 repeated_one |= repeated_one << i;
                 repeated_c |= repeated_c << i;
             }
@@ -97,53 +100,54 @@ void* core_rtl_memchr(const void* buf, u8 ch, size_t size)
 
     /*
     	Instead of the traditional loop which tests each byte, we will test a
-    	longword at a time.  The tricky part is testing if *any of the four*
-    	bytes in the longword in question are equal to ch.  We first use an xor
+    	longword_t at a time.  The tricky part is testing if *any of the four*
+    	bytes in the longword_t in question are equal to ch.  We first use an xor
     	with repeated_c.  This reduces the task to testing whether *any of the
-    	four* bytes in longword1 is zero.
+    	four* bytes in longword_t1 is zero.
 
     	We compute tmp =
-    		((longword1 - repeated_one) & ~longword1) & (repeated_one << 7).
+    		((longword_t1 - repeated_one) & ~longword_t1) & (repeated_one << 7).
     	That is, we perform the following operations:
     		1. Subtract repeated_one.
-    		2. & ~longword1.
+    		2. & ~longword_t1.
     		3. & a mask consisting of 0x80 in every byte.
     	Consider what happens in each byte:
-    		- If a byte of longword1 is zero, step 1 and 2 transform it into 0xff,
+    		- If a byte of longword_t1 is zero, step 1 and 2 transform it into 0xff,
     	and step 3 transforms it into 0x80.  A carry can also be propagated
     	to more significant bytes.
-    		- If a byte of longword1 is nonzero, let its lowest 1 bit be at
+    		- If a byte of longword_t1 is nonzero, let its lowest 1 bit be at
     	position k (0 <= k <= 7); so the lowest k bits are 0.  After step 1,
     	the byte ends in a single bit of value 0 and k bits of value 1.
     	After step 2, the result is just k bits of value 1: 2^k - 1.  After
     	step 3, the result is 0.  And no carry is produced.
-    	So, if longword1 has only non-zero bytes, tmp is zero.
-    	Whereas if longword1 has a zero byte, call j the position of the least
+    	So, if longword_t1 has only non-zero bytes, tmp is zero.
+    	Whereas if longword_t1 has a zero byte, call j the position of the least
     	significant zero byte.  Then the result has a zero at positions 0, ...,
     	j-1 and a 0x80 at position j.  We cannot predict the result at the more
     	significant bytes (positions j+1..3), but it does not matter since we
     	already have a non-zero bit at position 8*j+7.
 
-    	So, the test whether any byte in longword1 is zero is equivalent to
+    	So, the test whether any byte in longword_t1 is zero is equivalent to
     	testing whether tmp is nonzero.
     */
 
-    while(size >= sizeof(longword)) {
-        longword longword1 = *longword_ptr ^ repeated_c;
+    while(size >= sizeof(longword_t)) {
+        longword_t longword_t1 = *longword_t_ptr ^ repeated_c;
 
-        if((((longword1 - repeated_one) & ~longword1)
-            & (repeated_one << 7)) != 0)
+        if((((longword_t1 - repeated_one) & ~longword_t1)
+            & (repeated_one << 7)) != 0) {
             break;
+        }
 
-        longword_ptr++;
-        size -= sizeof(longword);
+        longword_t_ptr++;
+        size -= sizeof(longword_t);
     }
 
-    char_ptr = (u8*) longword_ptr;
+    char_ptr = (u8*) longword_t_ptr;
 
     /*
-    	At this point, we know that either size < sizeof (longword), or one of the
-    	sizeof (longword) bytes starting at char_ptr is == ch.  On little-endian
+    	At this point, we know that either size < sizeof (longword_t), or one of the
+    	sizeof (longword_t) bytes starting at char_ptr is == ch.  On little-endian
     	machines, we could determine the first such byte without any further
     	memory accesses, just by looking at the tmp result from the last loop
     	iteration.  But this does not work on big-endian machines.  Choose code
@@ -151,8 +155,9 @@ void* core_rtl_memchr(const void* buf, u8 ch, size_t size)
     */
 
     for(; size > 0; --size, ++char_ptr) {
-        if(*char_ptr == ch)
+        if(*char_ptr == ch) {
             return (void *) char_ptr;
+        }
     }
 
     return NULL;
@@ -160,8 +165,8 @@ void* core_rtl_memchr(const void* buf, u8 ch, size_t size)
 
 int core_rtl_memcmp(const void* buf1, const void* buf2, size_t size)
 {
-    longword* p_long1;
-    longword* p_long2;
+    longword_t* p_long1;
+    longword_t* p_long2;
     u8* p_byte1;
     u8* p_byte2;
     size_t n;
@@ -169,9 +174,9 @@ int core_rtl_memcmp(const void* buf1, const void* buf2, size_t size)
     u8 ret;
 
     //Compare the first few bytes to make the pointer aligned
-    if(size < sizeof(longword) * 2
-       || (address_t)buf1 % sizeof(longword)
-       != (address_t)buf2 % sizeof(longword)) {
+    if(size < sizeof(longword_t) * 2
+       || (address_t)buf1 % sizeof(longword_t)
+       != (address_t)buf2 % sizeof(longword_t)) {
         for(p_byte1 = (u8*)buf1, p_byte2 = (u8*)buf2, n = 0;
             n < size;
             p_byte1++, p_byte2++, n++) {
@@ -185,7 +190,7 @@ int core_rtl_memcmp(const void* buf1, const void* buf2, size_t size)
         return 0;
 
     } else {
-        len_to_cmp = (address_t)buf1 % sizeof(longword);
+        len_to_cmp = (address_t)buf1 % sizeof(longword_t);
         size -= len_to_cmp;
 
         for(p_byte1 = (u8*)buf1, p_byte2 = (u8*)buf2, n = 0;
@@ -199,22 +204,22 @@ int core_rtl_memcmp(const void* buf1, const void* buf2, size_t size)
         }
     }
 
-    //Compare sizeof(longword) bytes each time
-    p_long1 = (longword*)p_byte1;
-    p_long2 = (longword*)p_byte2;
-    //len_to_cmp = len_to_cmp / sizeof(longword) * sizeof(longword)
-    len_to_cmp = (~((sizeof(longword) - 1))) & size;
-    //size = size % sizeof(longword)
-    size = size & (sizeof(longword) - 1);
+    //Compare sizeof(longword_t) bytes each time
+    p_long1 = (longword_t*)p_byte1;
+    p_long2 = (longword_t*)p_byte2;
+    //len_to_cmp = len_to_cmp / sizeof(longword_t) * sizeof(longword_t)
+    len_to_cmp = (~((sizeof(longword_t) - 1))) & size;
+    //size = size % sizeof(longword_t)
+    size = size & (sizeof(longword_t) - 1);
 
     for(n = 0; n < len_to_cmp;
-        n += sizeof(longword), p_long1++, p_long2++) {
+        n += sizeof(longword_t), p_long1++, p_long2++) {
         if(*p_long1 - *p_long2 != 0) {
             p_byte1 = (u8*)p_long1;
             p_byte2 = (u8*)p_long2;
 
             for(n = 0;
-                n < sizeof(longword);
+                n < sizeof(longword_t);
                 n++, p_byte1++, p_byte2++) {
                 ret = *p_byte1 - *p_byte2;
 
@@ -488,9 +493,339 @@ void* core_rtl_memset(void* dest, u8 value, size_t size)
     return dest;
 }
 
-char* core_rtl_strchr(const char* str, char c);
-size_t core_rtl_strcspn(const char* str, const char* reject);
-size_t core_rtl_strlen(const char* str);
+char* core_rtl_strchr(const char* str, char c)
+{
+    //Copied from glibc
+    const unsigned char *char_ptr;
+    const longword_t *longword_ptr;
+    longword_t longword, magic_bits, charmask;
+    unsigned char chr;
+
+    chr = (unsigned char) c;
+
+    /*
+    	Handle the first few characters by reading one character at a time.
+        Do this until CHAR_PTR is aligned on a longword boundary.
+    */
+    for(char_ptr = (const unsigned char *) str;
+        ((unsigned long int) char_ptr & (sizeof(longword) - 1)) != 0;
+        ++char_ptr) {
+        if(*char_ptr == chr) {
+            return (void *) char_ptr;
+
+        } else if(*char_ptr == '\0') {
+            return NULL;
+        }
+    }
+
+    /*
+    	All these elucidatory comments refer to 4-byte longwords,
+        but the theory applies equally well to 8-byte longwords.
+    */
+
+    longword_ptr = (unsigned long int *) char_ptr;
+
+    /*
+    	Bits 31, 24, 16, and 8 of this number are zero.  Call these bits
+        the "holes."  Note that there is a hole just to the left of
+        each byte, with an extra at the end:
+
+        bits:  01111110 11111110 11111110 11111111
+        bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
+
+        The 1-bits make sure that carries propagate to the next 0-bit.
+        The 0-bits provide holes for carries to fall into.
+    */
+    switch(sizeof(longword)) {
+        case 4:
+            magic_bits = 0x7efefeffL;
+            break;
+
+        case 8:
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshift-overflow"
+            magic_bits = (((0x7efefefeL << 16)) << 16) | 0xfefefeffL;
+#pragma GCC diagnostic pop
+            break;
+
+            //default:
+            //TODO:Panic
+
+    }
+
+    /*
+    	Set up a longword, each of whose bytes is C.
+    */
+    charmask = chr | (chr << 8);
+    charmask |= charmask << 16;
+
+    if(sizeof(longword) > 4) {
+        /*
+        	Do the shift in two steps to avoid a warning if long has 32 bits.
+        */
+        charmask |= (charmask << 16) << 16;
+    }
+
+    if(sizeof(longword) > 8) {
+        //TODO:Panic
+    }
+
+    /*
+    	Instead of the traditional loop which tests each character,
+        we will test a longword at a time.  The tricky part is testing
+        if *any of the four* bytes in the longword in question are zero.
+    */
+    for(;;) {
+        /*
+        	We tentatively exit the loop if adding MAGIC_BITS to
+        	LONGWORD fails to change any of the hole bits of LONGWORD.
+
+        	1) Is this safe?  Will it catch all the zero bytes?
+            Suppose there is a byte with all zeros.  Any carry bits
+        	propagating from its left will fall into the hole at its
+        	least significant bit and stop.  Since there will be no
+        	carry from its most significant bit, the LSB of the
+        	byte to the left will be unchanged, and the zero will be
+        	detected.
+
+        	2) Is this worthwhile?  Will it ignore everything except
+        	zero bytes?  Suppose every byte of LONGWORD has a bit set
+        	somewhere.  There will be a carry into bit 8.  If bit 8
+        	is set, this will carry into bit 16.  If bit 8 is clear,
+        	one of bits 9-15 must be set, so there will be a carry
+        	into bit 16.  Similarly, there will be a carry into bit
+        	24.  If one of bits 24-30 is set, there will be a carry
+        	into bit 31, so all of the hole bits will be changed.
+
+        	The one misfire occurs when bits 24-30 are clear and bit
+        	31 is set; in this case, the hole at bit 31 is not
+        	changed.  If we had access to the processor carry flag,
+        	we could close this loophole by putting the fourth hole
+        	at bit 32!
+
+        	So it ignores everything except 128's, when they're aligned
+        	properly.
+
+        	3) But wait!  Aren't we looking for C as well as zero?
+        	Good point.  So what we do is XOR LONGWORD with a longword,
+        	each of whose bytes is C.  This turns each byte that is C
+        	into a zero.
+        */
+
+        longword = *longword_ptr++;
+
+        /*
+        	Add MAGIC_BITS to LONGWORD.
+        */
+        if((((longword + magic_bits)
+
+             /*
+              	Set those bits that were unchanged by the addition.
+             */
+             ^ ~longword)
+
+            /*
+            	Look at only the hole bits.  If any of the hole bits
+                are unchanged, most likely one of the bytes was a
+                zero.
+            */
+            & ~magic_bits) != 0 ||
+
+           /*
+           	That caught zeroes.  Now test for C.
+           */
+           ((((longword ^ charmask) + magic_bits) ^ ~(longword ^ charmask))
+            & ~magic_bits) != 0) {
+            /*
+            	Which of the bytes was C or zero?
+            	If none of them were, it was a misfire; continue the search.
+            */
+
+            const unsigned char *cp = (const unsigned char *)(longword_ptr - 1);
+
+            if(*cp == chr) {
+                return (char *) cp;
+
+            } else if(*cp == '\0') {
+                return NULL;
+            }
+
+            if(*++cp == chr) {
+                return (char *) cp;
+
+            } else if(*cp == '\0') {
+                return NULL;
+            }
+
+            if(*++cp == chr) {
+                return (char *) cp;
+
+            } else if(*cp == '\0') {
+                return NULL;
+            }
+
+            if(*++cp == chr) {
+                return (char *) cp;
+
+            } else if(*cp == '\0') {
+                return NULL;
+            }
+
+            if(sizeof(longword) > 4) {
+                if(*++cp == chr) {
+                    return (char *) cp;
+
+                } else if(*cp == '\0') {
+                    return NULL;
+                }
+
+                if(*++cp == chr) {
+                    return (char *) cp;
+
+                } else if(*cp == '\0') {
+                    return NULL;
+                }
+
+                if(*++cp == chr) {
+                    return (char *) cp;
+
+                } else if(*cp == '\0') {
+                    return NULL;
+                }
+
+                if(*++cp == chr) {
+                    return (char *) cp;
+
+                } else if(*cp == '\0') {
+                    return NULL;
+                }
+            }
+        }
+    }
+
+    return NULL;
+}
+
+size_t core_rtl_strcspn(const char* str, const char* reject)
+{
+    size_t ret;
+    char* p;
+
+    ret = 0;
+    p = (char*)str;
+
+    while(*p != '\0') {
+        if(core_rtl_strchr(reject, *(p++)) == NULL) {
+            ret++;
+
+        } else {
+            return ret;
+        }
+    }
+
+    return ret;
+}
+
+size_t core_rtl_strlen(const char* str)
+{
+    //Copied from glibc
+    const char *char_ptr;
+    const unsigned long int *longword_ptr;
+    unsigned long int longword, himagic, lomagic;
+
+    /*
+    	Handle the first few characters by reading one character at a time.
+    	Do this until CHAR_PTR is aligned on a longword boundary.
+    */
+    for(char_ptr = str; ((unsigned long int) char_ptr
+                         & (sizeof(longword) - 1)) != 0;
+        ++char_ptr) {
+        if(*char_ptr == '\0') {
+            return char_ptr - str;
+        }
+    }
+
+    /*
+    	All these elucidatory comments refer to 4-byte longwords,
+    	but the theory applies equally well to 8-byte longwords.
+    */
+
+    longword_ptr = (unsigned long int *) char_ptr;
+
+    /*
+    	Bits 31, 24, 16, and 8 of this number are zero.  Call these bits
+        the "holes."  Note that there is a hole just to the left of
+        each byte, with an extra at the end:
+
+        bits:  01111110 11111110 11111110 11111111
+        bytes: AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD
+
+        The 1-bits make sure that carries propagate to the next 0-bit.
+        The 0-bits provide holes for carries to fall into.
+    */
+    himagic = 0x80808080L;
+    lomagic = 0x01010101L;
+
+    if(sizeof(longword) > 4) {
+        //64-bit version of the magic.
+        //Do the shift in two steps to avoid a warning if long has 32 bits.
+        himagic = ((himagic << 16) << 16) | himagic;
+        lomagic = ((lomagic << 16) << 16) | lomagic;
+    }
+
+    if(sizeof(longword) > 8) {
+        //TODO:Panic
+    }
+
+    /*
+    	Instead of the traditional loop which tests each character,
+        we will test a longword at a time.  The tricky part is testing
+        if *any of the four* bytes in the longword in question are zero.
+    */
+    for(;;) {
+        longword = *longword_ptr++;
+
+        if(((longword - lomagic) & ~longword & himagic) != 0) {
+            /*
+            	Which of the bytes was the zero?  If none of them were, it was
+                a misfire; continue the search.
+            */
+
+            const char *cp = (const char *)(longword_ptr - 1);
+
+            if(cp[0] == 0) {
+                return cp - str;
+            }
+
+            if(cp[1] == 0) {
+                return cp - str + 1;
+            }
+
+            if(cp[2] == 0) {
+                return cp - str + 2;
+            }
+
+            if(cp[3] == 0) {
+                return cp - str + 3;
+            }
+
+            if(sizeof(longword) > 4) {
+                if(cp[4] == 0)
+                    return cp - str + 4;
+
+                if(cp[5] == 0)
+                    return cp - str + 5;
+
+                if(cp[6] == 0)
+                    return cp - str + 6;
+
+                if(cp[7] == 0)
+                    return cp - str + 7;
+            }
+        }
+    }
+}
+
 char* core_rtl_strncat(char *dest, const char *src, size_t len);
 int core_rtl_strncmp(const char* dest, const char* src, size_t len);
 char* core_rtl_strncpy(char* dest, const char* src, size_t len);
