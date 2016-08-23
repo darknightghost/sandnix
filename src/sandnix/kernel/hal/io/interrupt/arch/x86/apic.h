88f943eb0b8cfd696c7682ab2a23523096d5d36d
darknightghost <darknightghost.cn@gmail.com>
2016-08-23 11:35:36 +0800

    UTC:2016-08-23  3:35:36 : interrupt

--- src/sandnix/kernel/hal/io/interrupt/arch/x86/apic.h
+++ src/sandnix/kernel/hal/io/interrupt/arch/x86/apic.h
@@ -17,28 +17,4 @@
 
 #pragma once
 
-#include "../../../../../common/common.h"
 
-#if defined x86
-
-//IN
-u8		hal_io_in_8(address_t port);
-u16		hal_io_in_16(address_t port);
-u32		hal_io_in_32(address_t port);
-
-//INS
-void	hal_io_ins_8(void* dest, size_t count, address_t port);
-void	hal_io_ins_16(void* dest, size_t count, address_t port);
-void	hal_io_ins_32(void* dest, size_t count, address_t port);
-
-//OUT
-void	hal_io_out_8(address_t port, u8 data);
-void	hal_io_out_16(address_t port, u16 data);
-void	hal_io_out_32(address_t port, u32 data);
-
-//OUTS
-void	hal_io_outs_8(address_t port, size_t count, void* src);
-void	hal_io_outs_16(address_t port, size_t count, void* src);
-void	hal_io_outs_32(address_t port, size_t count, void* src);
-
-#endif
