921de6e78936dd831970b91d90def4d5a5e6abcb
darknightghost <darknightghost.cn@gmail.com>
2017-02-08 17:33:33 +0800

    UTC:2017-02-08  9:33:33 : schedule

--- src/sandnix/kernel/core/pm/thread/thread.c
+++ src/sandnix/kernel/core/pm/thread/thread.c
@@ -48,8 +48,8 @@ static	void				on_tick(u32 int_num, pcontext_t p_context,
 
 static	void				add_use_count(pcore_sched_info_t p_info,
         bool is_busy);
-static inline void			switch_task(pcore_sched_info_t p_info,
-                                        plist_node_t p_node);
+static inline void			next_task(pcore_sched_info_t p_info);
+static inline void			switch_task(pcore_sched_info_t p_info);
 
 void core_pm_thread_init()
 {
@@ -77,11 +77,10 @@ void core_pm_thread_init()
     pcore_sched_info_t p_info = &cpu_infos[0];
     p_info->cpu_use_stat_l = 0;
     p_info->cpu_use_stat_h = 0;
-    p_info->current_node = (plist_node_t)core_mm_heap_alloc(sizeof(list_node_t),
-                           sched_heap);
-    p_info->current_node->p_item = p_thread_obj;
+    INC_REF(p_thread_obj);
+    p_info->p_idle_thread = p_thread_obj;
     p_info->priority = PRIORITY_DISPATCH;
-    p_info->idle_thread_id = 0;
+    p_info->current_node = NULL;
     core_pm_spnlck_init(&(p_info->lock));
 
     //Initialize schedule list
@@ -102,6 +101,9 @@ void core_pm_thread_core_init()
     p_info->cpu_use_stat_l = 0;
     p_info->cpu_use_stat_h = 0;
     p_info->current_node = NULL;
+
+    //TODO://Create idle thread
+    NOT_SUPPORT;
     core_pm_spnlck_init(&(p_info->lock));
     p_info->enabled = true;
 
@@ -122,22 +124,29 @@ void core_pm_thread_core_release()
         core_pm_spnlck_unlock(&(p_info->lock));
 
         //Add Task to schedule list
-        core_pm_spnlck_lock(&sched_list_lock);
-        pthread_obj_t p_obj = (pthread_obj_t)(p_node->p_item);
-        plist_t p_list = &sched_lists[p_obj->priority];
+        pthread_obj_t p_thread_obj = (pthread_obj_t)(p_info->current_node->p_item);
 
-        if(*p_list == NULL) {
-            *p_list = p_node;
-            p_node->p_prev = p_node;
-            p_node->p_next = p_node;
+        if(p_thread_obj != p_info->p_idle_thread) {
+            core_pm_spnlck_lock(&sched_list_lock);
+            pthread_obj_t p_obj = (pthread_obj_t)(p_node->p_item);
+            plist_t p_list = &sched_lists[p_obj->priority];
 
-        } else {
-            p_node->p_prev = (*p_list)->p_prev;
-            p_node->p_next = (*p_list);
-            (*p_list)->p_prev->p_next = p_node;
-            (*p_list)->p_next->p_prev = p_node;
-            *p_list = p_node;
+            if(*p_list == NULL) {
+                *p_list = p_node;
+                p_node->p_prev = p_node;
+                p_node->p_next = p_node;
+
+            } else {
+                p_node->p_prev = (*p_list)->p_prev;
+                p_node->p_next = (*p_list);
+                (*p_list)->p_prev->p_next = p_node;
+                (*p_list)->p_next->p_prev = p_node;
+                *p_list = p_node;
+            }
         }
+
+        //TODO:Remove idle thread
+        NOT_SUPPORT;
 
         core_pm_spnlck_unlock(&sched_list_lock);
 
@@ -177,14 +186,21 @@ void on_tick(u32 int_num, pcontext_t p_context, u32 err_code)
         hal_cpu_context_load(p_context);
     }
 
-    pthread_obj_t p_thread_obj = NULL;
+    //Save context
+    pthread_obj_t p_thread_obj;
 
-    if(p_info->current_node != NULL) {
+    if(p_info->current_node == NULL) {
+        p_thread_obj = p_info->p_idle_thread;
+
+    } else {
         p_thread_obj = (pthread_obj_t)(p_info->current_node->p_item);
     }
 
     p_thread_obj->p_context = p_context;
 
+    //Switch task
+    next_task(p_info);
+    switch_task(p_info);
 
     UNREFERRED_PARAMETER(int_num);
     UNREFERRED_PARAMETER(err_code);
@@ -210,32 +226,84 @@ void add_use_count(pcore_sched_info_t p_info, bool is_busy)
     return;
 }
 
-void switch_task(pcore_sched_info_t p_info, plist_node_t p_node)
+void next_task(pcore_sched_info_t p_info)
 {
-    //Add current thread to schedule list
-    if(p_info->current_node != NULL) {
-        if(sched_lists[p_info->priority] == NULL) {
-            p_info->current_node->p_prev = NULL;
-            p_info->current_node->p_next = NULL;
-            sched_lists[p_info->priority] = NULL;
+    pthread_obj_t p_thread_obj = NULL;
 
-        } else {
-            p_info->current_node->p_prev = sched_lists[p_info->priority]
-                                           ->p_prev;
-            p_info->current_node->p_next = sched_lists[p_info->priority];
-            p_info->current_node->p_prev->p_next = p_info->current_node;
-            p_info->current_node->p_next->p_prev = p_info->current_node;
-        }
+    //Get current thread object
+    if(p_info->current_node != NULL) {
+        pthread_obj_t p_thread_obj = (pthread_obj_t)(p_info->current_node->p_item);
     }
 
-    //Switch task
+    //Search for next task
+    plist_node_t p_node = NULL;
+
+    if(p_thread_obj == NULL
+       || !(p_thread_obj->can_run(p_thread_obj))) {
+        //Check if current thread can be preempted
+        if(p_thread_obj->priority == PRIORITY_HIGHEST) {
+            //Highest priority, resume current thread
+            add_use_count(p_info, true);
+            return;
+        }
+
+        //Realtime tasks
+        core_pm_spnlck_lock(&sched_list_lock);
+        u32 end_priority = MAX(p_info->priority, PRIORITY_DISPATCH);
+
+        for(u32 pri = PRIORITY_HIGHEST;
+            pri >= end_priority;
+            pri--) {
+            if(pri == p_info->priority) {
+                continue;
+            }
+
+            if(sched_lists[pri] != NULL) {
+                p_node = sched_lists[pri];
+
+                if(p_node->p_prev == p_node) {
+                    sched_lists[pri] = NULL;
+
+                } else {
+                    p_node->p_prev->p_next = p_node->p_next;
+                    p_node->p_next->p_prev = p_node->p_prev;
+                    sched_lists[pri] = p_node->p_next;
+                }
+            }
+        }
+
+        //Normal tasks
+        if(p_node == NULL) {
+        }
+
+        //IDLE tasks
+        if(p_node == NULL) {
+        }
+
+        core_pm_spnlck_unlock(&sched_list_lock);
+
+    } else {
+        //Look for a thread to run
+    }
+
     p_info->current_node = p_node;
 
-    pthread_obj_t p_thread_obj = (pthread_obj_t)(p_info->current_node->p_item);
-    p_info->priority = p_thread_obj->priority;
+    return;
+}
 
-    //Load context
-    hal_cpu_context_load(p_thread_obj->p_context);
+void switch_task(pcore_sched_info_t p_info)
+{
+    pthread_obj_t p_thread_obj;
+
+    if(p_info->current_node == NULL) {
+        p_thread_obj = p_info->p_idle_thread;
+
+    } else {
+        p_thread_obj = (pthread_obj_t)(p_info->current_node->p_item);
+    }
+
+    p_info->priority = p_thread_obj->priority;
+    p_thread_obj->resume(p_thread_obj);
 
     return;
 }
